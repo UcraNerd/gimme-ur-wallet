d609cc3f73e9a85022b5d22e382a8fb5482637b8
PyperBox <102480084+UcraNerd@users.noreply.github.com>
2026-04-13 20:03:23 +0200

    Update Views

---
+++ GimmeUrWallet/src/gimmeurwallet/view/RTView.java
@@ -0,0 +1,47 @@
+package gimmeurwallet.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import gimmeurwallet.controller.RTController;
+
+public class RTView extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RTView frame = new RTView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public RTView() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+
+	}
+	
+	public void registerController(RTController c) {
+			
+	}
+}
--- GimmeUrWallet/src/gimmeurwallet/view/View.java
+++ GimmeUrWallet/src/gimmeurwallet/view/View.java
@@ -44,6 +44,7 @@ public class View extends JFrame {
 	public View() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1323, 784);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
