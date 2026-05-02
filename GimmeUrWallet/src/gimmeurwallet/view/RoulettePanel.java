fa3cbf4137e121218207e40181d7fccb1e41ea51
PyperBox <102480084+UcraNerd@users.noreply.github.com>
2026-05-02 13:41:40 +0200

    Update RoulettePanel.java

--- GimmeUrWallet/src/gimmeurwallet/view/RoulettePanel.java
+++ GimmeUrWallet/src/gimmeurwallet/view/RoulettePanel.java
@@ -11,7 +11,7 @@ import java.util.Random;
  */
 public class RoulettePanel extends JPanel implements Runnable {
     private RTController controller;
-    private JLabel walletLabel;
+    private final JLabel walletLabel;
     private final int[] numbers = {
             0,32,15,19,4,21,2,25,17,34,6,27,13,36,11,30,8,23,10,
             5,24,16,33,1,20,14,31,9,22,18,29,7,28,12,35,3,26
