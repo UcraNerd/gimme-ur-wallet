5eb13fd7df21b8b4d1cd4afc6e31eac16c6e2cc8
PyperBox <102480084+UcraNerd@users.noreply.github.com>
2026-05-02 13:33:59 +0200

    Update RTController.java

--- GimmeUrWallet/src/gimmeurwallet/controller/RTController.java
+++ GimmeUrWallet/src/gimmeurwallet/controller/RTController.java
@@ -29,9 +29,7 @@ public class RTController {
 		try {
 			double initialMoney = this.theGame.getWallet();
 			this.view.getRoulettePanel().updateWalletLabel(initialMoney);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		} catch (InterruptedException _) {}
 	}
 
 	/**
@@ -86,9 +84,7 @@ public class RTController {
 			double currentMoney = theGame.getWallet();
 			view.getRoulettePanel().updateWalletLabel(currentMoney);
 
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		} catch (InterruptedException _) {}
 
 		String message = (won > 0) ?
 				"VITTORIA!\nNumero uscito: " + drawn + "\nVinto: " + balance + "€" :
